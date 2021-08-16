2f54e7917dd228d95f19315737e1d5b5cf76d92a
Dmitriy Bratishchev <dmitriy-1008@yandex.ru>
2021-08-16 15:32:33 +0300

    added text

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const elem = <h1>Hello world ggg ffff</h1>;
+const elem = <h1>Hello World меня зовут: Дмитрий Братищев</h1>;
 ReactDom.render(elem, document.getElementById("root"));
